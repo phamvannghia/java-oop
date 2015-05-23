f0e2f4d040d3e81d63cfa9b4a78d9651c61fdda8
Admin <Admin@D20912>
2015-05-23 14:31:56 +0700

    Example01

---
+++ Example01/src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		Rectangle rect=new Rectangle();
+		Rectangle rectb=new Rectangle();
+		rect.setValues(3, 4);
+		rect.setValues(5, 6);
+		
+		System.out.println("rect a:"+ rect.area());
+		System.out.println("rect b:"+ rectb.area());
+	}
+
+}
